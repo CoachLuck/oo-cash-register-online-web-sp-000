b11720477fa2609c63056af5fdc40cf125719928
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-25 06:07:12 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
+class Person
+  attr_accessor :age
 
+  def initialize(age = 0)
+    @age = age
+  end
+
+  def birthday
+    self.age += 1
+  end
+end
+
+class CashRegister
+  attr_accessor :total, :items
+  @items = []
+
+  def initialize(discount = 0)
+    @total = 0;
+  end
+
+  def add_item(title, price, amt = 1)
+    item = {title: title, price: price: amt}
+    @items << item
+  end
+end
