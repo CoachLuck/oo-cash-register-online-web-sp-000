565bbdc9fa104cf3c0837119673057cdd06d9c68
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-25 06:28:53 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -22,7 +22,7 @@ class CashRegister
   def add_item(title, price, amt = 1)
     self.total += price * amt
     item = {title: title, price: price, amount: amt}
-    amt.do
+    amt.loop
       self.items << title
     end
   end
