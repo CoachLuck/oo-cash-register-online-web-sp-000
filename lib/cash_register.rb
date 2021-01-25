07cc0306fc43928495899c4e64e003a17cc3535d
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-25 06:23:28 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -27,7 +27,7 @@ class CashRegister
 
   def apply_discount
     return puts "There is no discount to apply" if @discount <= 0
-    (@total) / (100 - @discount)
+    @total = (@total) / (100 - @discount)
   end
 
   def items
